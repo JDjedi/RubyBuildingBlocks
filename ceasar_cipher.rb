dbd605848122364089993d4989e6b296cb67a80b
Glenn <gcrosby15@email.com>
2015-05-29 16:52:07 +0800

    Fixed some bugs in caesar_cipher. Now it is translating correctly.

--- ceasar_cipher.rb
+++ ceasar_cipher.rb
@@ -36,24 +36,25 @@ def caesar_mapping(input, low_value, high_value)
 	if input <= high_value
 		result = input
 	else
-		result = input%high_value + low_value - 1
+		result = (input-low_value) % (high_value-low_value+1) + low_value
 	end
 	return result
 end
 
 def caesar_cipher(original_text, shift_value)
 	converted_text = original_text
-
 	original_text.length.times do |idx|
-	# original_text.scan /./ do |letter|
 		letter = original_text[idx]
-		# Begin encoding each letter. Any non letter is not converter.
 
+		# Begin encoding each letter. Any non letter is not converter.
 		# ascii codes:
 		# A - 65
 		# Z - 90
 		# a - 97
 		# z - 122
+
+		# 01234567890123456789012345
+		# ABCDEFGHIJKLMNOPQRSTUVWXYZ
 
 		letter_int_value = letter.ord
 		if letter_int_value >= 65 && letter_int_value <= 90
@@ -70,4 +71,20 @@ def caesar_cipher(original_text, shift_value)
 	puts converted_text
 end
 
+# Some testing of caesar_cipher()
 caesar_cipher("What a string!", 5)
+caesar_cipher("What a string!", 31)
+caesar_cipher("What a string!", 26*50+5)
+
+
+# # some testing of caesar_mapping()
+# begin_number = 65
+# 65.upto(90) do |number|
+# 	number_map = number + 36 + 26*5
+
+# 	# number_map = number + 26*100
+# 	# number_map = number + 1
+
+# 	puts "#{number} - #{number.chr} : #{number_map} - #{caesar_mapping(number_map, 65, 90).chr}"
+# end
+
